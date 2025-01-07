ec5088184e9f5c2bdbd5e67644753503a0dd43b1
hpc5433 <christinameier07@gmail.com>
2025-01-07 12:05:56 -0500

    edits

--- dataGenerato.R
+++ dataGenerato.R
@@ -3,20 +3,18 @@
 #Question 1
 
 #create a list of 5 random species name
-speciesNames <- c("cat", "dog", "rabbit", "fish", "reptile")  
-
-#test
+DogTypes_5 <- c("GoldenRetriever", "GermanSheppard", "Labrador", "Collie", "AustralianSheppard")  
 
 #sample from the speciesNames vector a new vector of 100 random ones
-Species <- sample(speciesNames, 100, replace = TRUE)
+DogTypes <- sample(DogTypes_5, 100, replace = TRUE)
 
 #Question 2
-#create a vector called Limb_width that is 100 rows long and has min and max values between 1-5
-Limb_width <- runif(100, min = 1, max = 5)
+#create a vector called Limb_width that is 100 rows long and has min and max values between 3-8cm
+Limb_width <- runif(100, min = 3, max = 8)
 
 #Question 3
-#create a vector called Limb_length that is 100 rows long and has min and max values between 5-150
-Limb_length <- runif(100, min = 5, max = 150)
+#create a vector called Limb_length that is 100 rows long and has min and max values between 40-60cm
+Limb_length <- runif(100, min = 40, max = 60)
 
 #Question 4
 #create a vector of 3 names to randomly sample from
@@ -27,7 +25,7 @@ Observer <- sample(observer_options, 100, replace = TRUE)
 
 #Question 5
 #put all vectors into a dataframe
-measurements_df <- data.frame(Species, Limb_width, Limb_length, Observer)
+measurements_df <- data.frame(DogTypes, Limb_width, Limb_length, Observer)
 
 #Make csv
 #write to csv
---
+++ volumeEstimato.R
@@ -0,0 +1,10 @@
+library(dplyr)
+
+#read in measurements
+measurements <- read.csv("measurements.csv")
+
+#calculate volume in cm^3
+measurements <- measurements %>% 
+  select(-X) %>%
+  mutate(Volume = pi * (Limb_width / 2)^2 * Limb_length)
+
